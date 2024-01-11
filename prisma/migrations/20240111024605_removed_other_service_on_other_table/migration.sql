1cff4f4f9903b54ed8b7fc5ad4e2b922cd62f3d7
MatthewRomero007 <113237170+MatthewRomero007@users.noreply.github.com>
2024-01-11 14:00:27 +0800

    tinanggal lang ung otherService sa prisma models

---
+++ prisma/migrations/20240111024605_removed_other_service_on_other_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `otherservice` on the `servicefeedback` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `servicefeedback` DROP COLUMN `otherservice`;
