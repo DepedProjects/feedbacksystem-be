e511b7fcdbcd5eff70f6be1f2e793bd3329148b7
Mark Joseph Salem <125126248+MarkSalem804@users.noreply.github.com>
2024-05-09 15:57:29 +0800

    ...

--- src/middlewares/corsConfig/allowedOrigins.js
+++ src/middlewares/corsConfig/allowedOrigins.js
@@ -6,6 +6,10 @@ const allowedOrigins = [
   "http://210.213.155.70:3000",
   "http://127.0.0.1:5500",
   "https://feedback.depedimuscity.com:8030",
+  "https://synergy.depedimuscity.com:8021",
+  "http://172.16.0.21:8021",
+  "https://172.16.0.21:8021",
+  "http://localhost:8021",
 ];
 
 module.exports = allowedOrigins;
