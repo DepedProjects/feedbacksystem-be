8086cc1661d8a0afc79de9a4bc7fc4b9d063866f
Mark Joseph Salem <125126248+MarkSalem804@users.noreply.github.com>
2024-05-09 15:58:11 +0800

    ..

--- src/middlewares/corsConfig/allowedOrigins.js
+++ src/middlewares/corsConfig/allowedOrigins.js
@@ -7,6 +7,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",
   "https://feedback.depedimuscity.com:8030",
   "https://synergy.depedimuscity.com:8021",
+  "http://synergy.depedimuscity.com:8021",
   "http://172.16.0.21:8021",
   "https://172.16.0.21:8021",
   "http://localhost:8021",
